04ff1e3973d9ce86f6b699f8b1eb941631d69c0f
RominaMaita <u202213765@upc.edu.pe>
2024-09-02 21:50:01 -0500

    feat(news): added article list component.

---
+++ src/app/news/components/article-item/article-item.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input} from '@angular/core';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitleGroup} from "@angular/material/card";
+
+@Component({
+  selector: 'app-article-item',
+  standalone: true,
+  imports: [
+    MatCardActions,
+    MatCard,
+    MatCardHeader,
+    MatCardTitleGroup,
+    MatCardContent
+  ],
+  templateUrl: './article-item.component.html',
+  styleUrl: './article-item.component.css'
+})
+export class ArticleItemComponent {
+  @Input() article!: Article;
+
+
+}
---
+++ src/app/news/components/article-list/article-list.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+import {ArticleItemComponent} from "../article-item/article-item.component";
+
+@Component({
+  selector: 'app-article-list',
+  standalone: true,
+  imports: [
+    ArticleItemComponent
+  ],
+  templateUrl: './article-list.component.html',
+  styleUrl: './article-list.component.css'
+})
+export class ArticleListComponent {
+  title='CatchUp';
+
+  @Input() articles!: Array<Article>;
+
+}
