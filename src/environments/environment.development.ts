3fff0d4fb043f6f7960a0c536b78851177aa25e2
RominaMaita <u202213765@upc.edu.pe>
2024-09-04 11:28:00 -0500

    feat(environment): added environment variable support.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+//environment: development
+// This is development environment configuration file.
+// note: in real scenarios, this file is not commited to the repository
+export const environment = {
+  production: false,
+  newsApiKey: 'c77d4e4095ec4bcaa5d129a24897f6be'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  newsApiKey: 'c77d4e4095ec4bcaa5d129a24897f6be'
+};
+
